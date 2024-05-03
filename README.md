0915ed10e4b3d5001530facc98cd119f99646db0
Maci <traghettimaximiliano64@gmail.com>
2024-05-03 10:09:46 -0300

    se realizo y chequeo el archivo diaSemana.js

--- README.md
+++
@@ -1,1 +0,0 @@
-# TP-JavaScript-Nombre-Alumno-
---
+++ assets/JS/diaSemana.js
@@ -0,0 +1,19 @@
+let diaSemana = parseInt(prompt("ingrese un Número del 1 al 7:"));
+
+if (diaSemana==1){
+    console.log("Es Lunes")
+}else if (diaSemana==2){
+    console.log("Es Martes")
+}else if (diaSemana==3){
+    console.log("Es Miercoles")
+}else if (diaSemana==4){
+    console.log("Es Jueves")
+}else if (diaSemana==5){
+    console.log("Es Viernes")
+}else if (diaSemana==6){
+    console.log("Es Sabado")
+}else if (diaSemana==7){
+    console.log("Es Domingo")
+}else if (diaSemana>7){
+    console.log("Numero no valido.")
+}
