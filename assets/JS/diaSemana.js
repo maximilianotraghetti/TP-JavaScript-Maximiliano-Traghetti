a5c4ec3db576aeb19c84b5307ecb982fc2e6e7e5
Maci <traghettimaximiliano64@gmail.com>
2024-05-06 10:18:55 -0300

    Se corrigieron erroes y se cambio el formato a Switch, versión final de diaSemana.js

--- assets/JS/diaSemana.js
+++ assets/JS/diaSemana.js
@@ -1,19 +1,40 @@
 let diaSemana = parseInt(prompt("ingrese un Número del 1 al 7:"));
 
-if (diaSemana==1){
-    console.log("Es Lunes")
-}else if (diaSemana==2){
-    console.log("Es Martes")
-}else if (diaSemana==3){
-    console.log("Es Miercoles")
-}else if (diaSemana==4){
-    console.log("Es Jueves")
-}else if (diaSemana==5){
-    console.log("Es Viernes")
-}else if (diaSemana==6){
-    console.log("Es Sabado")
-}else if (diaSemana==7){
-    console.log("Es Domingo")
-}else if (diaSemana>7){
-    console.log("Numero no valido.")
+switch (diaSemana) {
+    case 1: diaSemana==1;
+        console.log("Es Lunes.")
+        
+        break;
+    case 2: diaSemana==2;
+        console.log("Es Martes.")
+        
+    break;
+
+    case 3: diaSemana==3;
+        console.log("Es Miercoles.")
+            
+    break;
+
+    case 4: diaSemana==4;
+        console.log("Es Jueves.")
+            
+    break;
+
+    case 5: diaSemana==5;
+        console.log("Es Viernes.")
+            
+    break;
+
+    case 6: diaSemana==6;
+        console.log("Es Sabado.")
+            
+    break;
+    case 7: diaSemana==7;
+        console.log("Es Domingo.")
+            
+    break;
+
+    default:
+        console.log("Ingrese un día Valido.")
+    break;
 }
